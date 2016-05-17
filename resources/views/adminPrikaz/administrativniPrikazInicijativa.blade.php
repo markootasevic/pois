b323552411794b4fb295ae19bb5964876de0837d
nikola <icarusrevolution88@gmail.com>
2016-05-17 15:24:49 +0200

    w3schools tab

--- resources/views/adminPrikaz/administrativniPrikazInicijativa.blade.php
+++ resources/views/adminPrikaz/administrativniPrikazInicijativa.blade.php
@@ -1,9 +1,91 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+body {font-family: "Lato", sans-serif;}
 
 
-<div>proba</div>
+ul.tab {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    border: 1px solid #ccc;
+    background-color: #f1f1f1;
+}
+
+/* Float the list items side by side */
+ul.tab li {float: left;}
+
+/* Style the links inside the list items */
+ul.tab li a {
+    display: inline-block;
+    color: black;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    transition: 0.3s;
+    font-size: 17px;
+}
+
+/* Change background color of links on hover */
+ul.tab li a:hover {
+    background-color: #ddd;
+}
+
+/* Create an active/current tablink class */
+ul.tab li a:focus, .active {
+    background-color: #ccc;
+}
+
+/* Style the tab content */
+.tabcontent {
+    display: none;
+    padding: 6px 12px;
+    border: 1px solid #ccc;
+    border-top: none;
+}
+</style>
+<body>
+
+<p>Click on the links inside the tabbed menu:</p>
+
+<ul class="tab">
+  <li><a href="#" class="tablinks" onclick="openCity(event, 'JunkInicijative')">incijative</a></li>
+  <li><a href="#" class="tablinks" onclick="openCity(event, 'Paris')">Prihvaćene inicijative</a></li>
+  <li><a href="#" class="tablinks" onclick="openCity(event, 'Tokyo')">Dodaj/Obriši nalog</a></li>
+</ul>
+
+<div id="London" class="tabcontent">
+  <h3>London</h3>
+  <p>London is the capital city of England.</p>
+</div>
+
+<div id="Paris" class="tabcontent">
+  <h3>Paris</h3>
+  <p>Paris is the capital of France.</p> 
+</div>
+
+<div id="Tokyo" class="tabcontent">
+  <h3>Toky</h3>
+  <p>Tokyo is the capital of Japan.</p>
+</div>
+
+<script>
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tabcontent.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+</script>
 
 
 
